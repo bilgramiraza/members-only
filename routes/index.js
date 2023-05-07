07c862ff97d6c5f509e1b2a803ff9f28445c4812
bilgramiraza <bilgramiraza@yahoo.com>
2023-05-07 11:45:20 +0530

    Setup Basic Route boilerplate for Posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+const postController = require('../controllers/postController');
+
+//POSTS ROUTES //
+//Website HomePage
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//Post Creation Page
+router.get('/post/create', postController.postCreateGet);
+router.post('/post/create', postController.postCreatePost);
+
+//Post Deletion Page
+router.get('/post/:id/delete', postController.postDeleteGet);
+router.post('/post/:id/delete', postController.postDeletePost);
+
+//Post Update Page
+router.get('/post/:id/update', postController.postUpdateGet);
+router.post('/post/:id/update', postController.postUpdatePost);
+
+//Post Details Page
+router.get('/post/:id', postController.postDetail);
+
+//List All posts
+router.get('/posts', postController.postList);
 
 module.exports = router;
