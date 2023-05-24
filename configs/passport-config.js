ebb1bb349cd24e469d405dbe705e5fc73d11496c
bilgramiraza <bilgramiraza@yahoo.com>
2023-05-25 00:16:56 +0530

    Implemented the user model in passport boilerplate

--- configs/passport-config.js
+++ configs/passport-config.js
@@ -1,9 +1,17 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 const verifyCallback = async (username, password, done)=>{
   try{
+    const foundUser = await User.fineOne({username}).lean();
+    if(!foundUser) return done(null, false, {message:'User not found'});
+
+    const passwordCompare = await bcrypt.compare(password, foundUser.password);
+    if(!passwordCompare)  return done(null, false, {message:'Invalid Password'});
+
+    return done(null, foundUser);
   }catch(err){
     return done(err);
   }
@@ -19,6 +27,8 @@ passport.serializeUser((user, done)=>{
 
 passport.deserializeUser(async (userId, done)=>{
   try{
+    const user = await User.findById(userId).lean();
+    return done(null, user);
   }catch(err){
     return done(err);
   }
