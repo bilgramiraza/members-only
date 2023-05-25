4852ca124fa9fd962fa6e9ad6fe8bfa7b021029f
bilgramiraza <bilgramiraza@yahoo.com>
2023-05-25 23:28:41 +0530

    Signup Controller Setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,35 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 exports.signUpGet= (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Sign Up Get Page');
+  return res.render('signUp');
 };
 
-exports.signUpPost= (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Sign Up Post Page');
+exports.signUpPost= async (req, res, next) => {
+  const {username, firstName, lastName, password} = req.body;
+  if(req.errorObject){
+    return res.render('signUp',{
+      username,
+      firstName,
+      lastName,
+      password,
+      errors:req.errorObject,
+    });
+  }
+  try{
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password,saltRounds);
+    const user = new User({
+      username,
+      password:hashedPassword,
+      firstName,
+      lastName,
+    });
+    await user.save();
+    return res.redirect('/');
+  }catch(err){
+    return next(err);
+  }
 };
 
 exports.loginGet= (req, res, next) => {
