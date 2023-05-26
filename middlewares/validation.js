29be6c2da2d137f8cd1a15382818b9dd7969bef0
bilgramiraza <bilgramiraza@yahoo.com>
2023-05-26 10:19:47 +0530

    Setup Validation for User Signup

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,4 +1,5 @@
 const { body, validationResult } = require('express-validator');
+const User = require('../models/user');
 
 function validationObject(req, res, next){
   //Create an ErrorHandling Object in the format of 
@@ -11,7 +12,29 @@ function validationObject(req, res, next){
   return next();
 }
 
+module.exports.isAuth = (req, res, next)=>{
+  if(req.isAuthenticated()) next();
+  else  res.render('login');
+};
+
+const duplicateUserCheck = async (value)=>{
+  const foundUser = await User.exists({username:value});
+  if(foundUser) return Promise.reject('User Already Exists. Please Choose a new username');
+  return true;
+};
+
+const confirmPassword = (value, { req })=> value === req.body.password;
+
 exports.postValidation = [
   body('post', 'Post Cannot be Blank').trim().isLength({min:1}).escape(),
+  validationObject,
+];
+
+exports.signUpValidation = [
+  body('username', 'Username Cannot be Blank').trim().isLength({min:1}).escape().custom(duplicateUserCheck),
+  body('firstName', 'First Name Cannot be Blank').trim().isLength({min:1}).escape(),
+  body('lastName', 'Last Name Cannot be Blank').trim().isLength({min:1}).escape(),
+  body('password', 'Password Cannot be Blank').trim().isLength({min:8}).escape(),
+  body('passwordConfirm', 'Password Cannot be Blank').trim().isLength({min:8}).custom(confirmPassword).escape(),
   validationObject,
 ];
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+const { signUpValidation } = require('../middlewares/validation');
 
 //USER ROUTES //
 /*User Sign up Route*/
 router.get('/sign-up', userController.signUpGet);
-router.post('/sign-up', userController.signUpPost);
+router.post('/sign-up', signUpValidation, userController.signUpPost);
 
 /*User Login Route*/
 router.get('/login', userController.signUpGet);
